29107e66236119fe6f0f834b37a18c14501a4f7b
Khizunov Anton <akhizuno@Khizunovs-MacBook-Pro.local>
2018-10-14 20:42:48 +0300

    1

--- tests/tax-tests.js
+++ tests/tax-tests.js
@@ -17,21 +17,6 @@ describe('tax', function() {
     });
   });
 
-  it('calculate() should send the subtotal in the request', function(done) {
-    nock('https://some-tax-service.com')
-      .post('/request')
-      .reply(200, function(uri, requestBody) {
-        return {
-          amount: JSON.parse(requestBody).subtotal * 0.10
-        };
-      });
-
-    tax.calculate(100, 'CA', function(taxDetails) {
-      expect(taxDetails).to.eql({ amount: 10 });
-      done();
-    });
-  });
-
   it('calculate() should not make a request if the state is not CA', function(done) {
     nock('https://some-tax-service.com')
       .post('/request')
